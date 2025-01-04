39a79e804fc24fc8999130c809b851da03cd73ec
moweishi <pujaqkinartem@gmail.com>
2025-01-04 03:47:24 +0300

    add log service
    log service contains:
    -getLogs(returns list of logs)
    -getGraph(returns total sales and total quantities and date)
    -validation for date

---
+++ backend/tests/logs/logs_test.go
@@ -0,0 +1,39 @@
+package tests
+
+import (
+	"testing"
+
+	"pim-sys/tests/assistance"
+	suite "pim-sys/tests/logs/suite"
+
+	proto "pim-sys/gen/go/logs"
+
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc/metadata"
+)
+
+const configPath = "suite/config.yaml"
+
+func TestRegisterLogin_Login_HappyPath(t *testing.T) {
+	token := assistance.GetTestToken(t)
+	ctx, st := suite.New(t, configPath)
+	ctx = metadata.AppendToOutgoingContext(ctx, "authorization", token)
+
+	newLog, err := st.LogsClient.GetLogs(ctx, &proto.GetLogsRequest{
+		ProductId: 1,
+	})
+	require.NoError(t, err)
+
+	require.NotNil(t, newLog)
+
+	newSales, err := st.LogsClient.GetGraph(ctx, &proto.GetGraphRequest{
+		Interval: 0,
+		DateFrom: 1704074400, //01.01.2024 02.00.00
+		DateTo:   1735704000, //01.01.2025 02.00.00
+	})
+	require.NoError(t, err)
+
+	require.NotNil(t, newSales)
+
+	require.Equal(t, 60, int(newSales.Graphs[0].TotalSales))
+}

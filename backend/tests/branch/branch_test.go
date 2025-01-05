4cdfb4594c68aca657368af82d0a5c8b02b37094
mihyilo <90507951+mihyilo@users.noreply.github.com>
2025-01-05 17:05:33 +0300

    added branch service (#26)
    branch service contains:
    -creating new branch
    -altering branch
    -deleting branch
    -getting list of branches
    bonuse:
    -varchar(128) for "site" in table branch

---
+++ backend/tests/branch/branch_test.go
@@ -0,0 +1,62 @@
+package tests
+
+import (
+	"testing"
+
+	"pim-sys/tests/assistance"
+	suite "pim-sys/tests/branch/suite"
+
+	proto "pim-sys/gen/go/branch"
+
+	"github.com/brianvoe/gofakeit/v6"
+	"github.com/stretchr/testify/require"
+	"golang.org/x/exp/rand"
+	"google.golang.org/grpc/metadata"
+)
+
+const configPath = "suite/config.yaml"
+
+func TestRegisterLogin_Login_HappyPath(t *testing.T) {
+	token := assistance.GetTestToken(t)
+	ctx, st := suite.New(t, configPath)
+	ctx = metadata.AppendToOutgoingContext(ctx, "authorization", token)
+	types := [3]string{"online", "marketplace", "offline"}
+	respNewShop, err := st.BranchClient.NewBranch(ctx, &proto.NewBranchRequest{
+		Name:        gofakeit.FirstName(),
+		ShopId:      1,
+		Description: gofakeit.LastName(),
+		Address:     gofakeit.Address().Address,
+		Site:        gofakeit.URL(),
+		BranchType:  types[rand.Intn(2)+1],
+	})
+	require.NoError(t, err)
+
+	require.NotNil(t, respNewShop)
+
+	respAlterShop, err := st.BranchClient.AlterBranch(ctx, &proto.AlterBranchRequest{
+		BranchId: 1,
+		BranchInfo: &proto.BranchInfo{
+			Name:        "new-name",
+			Description: "new-description",
+			Address:     "new-address",
+			Site:        "new-site",
+		},
+	})
+	require.NoError(t, err)
+
+	require.NotNil(t, respAlterShop)
+
+	respListShops, err := st.BranchClient.ListBranches(ctx, &proto.ListBranchesRequest{
+		ShopId: 1,
+	})
+	require.NoError(t, err)
+	require.Equal(t, "new-name", respListShops.GetInfo()[0].Name)
+
+	respDeleteShop, err := st.BranchClient.DeleteBranch(ctx, &proto.DeleteBranchRequest{
+		BranchId: 1,
+	})
+	require.NoError(t, err)
+
+	require.NotNil(t, respDeleteShop)
+
+}

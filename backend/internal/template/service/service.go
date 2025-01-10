1a557c22d3e0c1521abf05760807dcd19672946d
Anatoliy Prohortsev <tolik.spb.ru@yandex.ru>
2025-01-10 20:30:51 +0300

    changed paths=)

---
+++ backend/internal/template/service/service.go
@@ -0,0 +1,132 @@
+package template_service
+
+import (
+	"context"
+	"fmt"
+	proto "pim-sys/gen/go/template"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type ServerAPI struct {
+	proto.UnimplementedTemplateServer // Хитрая штука, о ней ниже
+	template                          Template
+}
+
+// Тот самый интерфейс, котрый мы передавали в grpcApp
+type Template interface {
+	NewTemplate(
+		ctx context.Context,
+		name string,
+		description string,
+		branch_id int32,
+		attributes []*proto.AttributeInfo,
+	) error
+	AlterTemplate(
+		ctx context.Context,
+		template_id int32,
+		name string,
+		description string,
+		attributes []*proto.AttributeInfo,
+	) error
+	DeleteTemplate(
+		ctx context.Context,
+		template_id int32,
+	) error
+	ListTemplates(
+		ctx context.Context,
+	) (
+		[]*proto.TemplateInfo,
+		error,
+	)
+}
+
+func Register(gRPCServer *grpc.Server, template Template) {
+	proto.RegisterTemplateServer(gRPCServer, &ServerAPI{template: template})
+}
+
+func (s *ServerAPI) NewTemplate(
+	ctx context.Context,
+	in *proto.NewTemplateRequest,
+) (*proto.NewTemplateResponse, error) {
+	if in.Name == "" {
+		return nil, status.Error(codes.InvalidArgument, "name is required")
+	}
+	if in.Description == "" {
+		return nil, status.Error(codes.InvalidArgument, "description is required")
+	}
+	if in.BranchId == 0 {
+		return nil, status.Error(codes.InvalidArgument, "branch id is required")
+	}
+	if len(in.Attributes) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "attribute structures is required")
+	}
+
+	err := s.template.NewTemplate(
+		ctx,
+		in.GetName(),
+		in.GetDescription(),
+		in.GetBranchId(),
+		in.GetAttributes())
+
+	if err != nil {
+		return nil, status.Error(codes.Internal, fmt.Errorf("failed to create new template: %w", err).Error())
+	}
+
+	return &proto.NewTemplateResponse{}, nil
+}
+
+func (s *ServerAPI) AlterTemplate(
+	ctx context.Context,
+	in *proto.AlterTemplateRequest,
+) (*proto.AlterTemplateResponse, error) {
+	if in.BranchId == 0 {
+		return nil, status.Error(codes.InvalidArgument, "branch_id is required")
+	}
+	if in.Name == "" {
+		return nil, status.Error(codes.InvalidArgument, "name is required")
+	}
+	if in.Description == "" {
+		return nil, status.Error(codes.InvalidArgument, "description is required")
+	}
+	if len(in.Attributes) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "template id is required")
+	}
+
+	err := s.template.AlterTemplate(ctx, in.GetBranchId(), in.GetName(), in.GetDescription(), in.GetAttributes())
+	if err != nil {
+		return nil, status.Error(codes.Internal, fmt.Errorf("failed to alter shop: %w", err).Error())
+	}
+
+	return &proto.AlterTemplateResponse{}, nil
+}
+
+func (s *ServerAPI) DeleteTemplate(
+	ctx context.Context,
+	in *proto.DeleteTemplateRequest,
+) (*proto.DeleteTemplateResponse, error) {
+	if in.TemplateId == 0 {
+		return nil, status.Error(codes.InvalidArgument, "template id is required")
+	}
+
+	err := s.template.DeleteTemplate(ctx, in.GetTemplateId())
+	if err != nil {
+		return nil, status.Error(codes.Internal, fmt.Errorf("failed to delete template: %w", err).Error())
+	}
+
+	return &proto.DeleteTemplateResponse{}, nil
+}
+
+func (s *ServerAPI) ListTemplates(
+	ctx context.Context,
+	in *proto.ListTemplatesRequest,
+) (*proto.ListTemplatesResponse, error) {
+	templateInfo, err := s.template.ListTemplates(ctx)
+	if err != nil {
+		return nil, status.Error(codes.Internal, fmt.Errorf("failed to delete template: %w", err).Error())
+	}
+
+	return &proto.ListTemplatesResponse{Info: templateInfo}, nil
+}

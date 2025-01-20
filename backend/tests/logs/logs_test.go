47a701948eaeb281317f87f33bc023ab583247c8
Anatoliy Prohortsev <tolik.spb.ru@yandex.ru>
2025-01-20 18:15:52 +0300

    add db trigger function product_update_log()

--- backend/tests/logs/logs_test.go
+++ backend/tests/logs/logs_test.go
@@ -37,3 +37,15 @@ func TestRegisterLogin_Login_HappyPath(t *testing.T) {
 
 	require.Equal(t, 160, int(newSales.Graphs[0].TotalSales))
 }
+
+func TestLogUpdateProduct(t *testing.T) {
+	token := assistance.GetTestToken(t)
+	ctx, st := suite.New(t, configPath)
+	ctx = metadata.AppendToOutgoingContext(ctx, "authorization", token)
+
+	udpdateLog, err := st.LogsClient.GetLogs(ctx, &proto.GetLogsRequest{
+		ProductId: 1,
+	})
+	require.NoError(t, err)
+	require.Equal(t, len(udpdateLog.GetLogs()), 2)
+}

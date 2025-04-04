54b9e4452f001a8e6ff4fc47edb19e3bff69f4fc
mihyilo <miha170303@gmail.com>
2025-04-04 21:53:18 +0300

    add test
    1 integration test

--- backend/tests/logs/logs_test.go
+++ backend/tests/logs/logs_test.go
@@ -8,6 +8,7 @@ import (
 
 	proto "pim-sys/gen/go/logs"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc/metadata"
 )
@@ -37,15 +38,16 @@ func TestRegisterLogin_Login_HappyPath(t *testing.T) {
 
 	require.Equal(t, 160, int(newSales.Graphs[0].TotalSales))
 }
-
-func TestLogUpdateProduct(t *testing.T) {
+func TestGraphInvalidDate(t *testing.T) {
 	token := assistance.GetTestToken(t)
 	ctx, st := suite.New(t, configPath)
 	ctx = metadata.AppendToOutgoingContext(ctx, "authorization", token)
 
-	udpdateLog, err := st.LogsClient.GetLogs(ctx, &proto.GetLogsRequest{
-		ProductId: 1,
+	_, err := st.LogsClient.GetGraph(ctx, &proto.GetGraphRequest{
+		Interval: 0,
+		DateFrom: 0,
+		DateTo:   1735704000, //01.01.2025 02.00.00
 	})
-	require.NoError(t, err)
-	require.Equal(t, 4, len(udpdateLog.GetLogs()))
+	assert.ErrorContains(t, err, "DateFrom can't be less than minimal date of sales")
+
 }

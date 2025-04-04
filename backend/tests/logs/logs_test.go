cad7d7e9943bd74487b7c6ba9be87a31d035cea2
AnatoliyProhortsev <tolik.spb.ru@yandex.ru>
2025-04-04 20:59:14 +0300

    Added two negative tests

--- backend/tests/logs/logs_test.go
+++ backend/tests/logs/logs_test.go
@@ -38,14 +38,29 @@ func TestRegisterLogin_Login_HappyPath(t *testing.T) {
 	require.Equal(t, 160, int(newSales.Graphs[0].TotalSales))
 }
 
-func TestLogUpdateProduct(t *testing.T) {
+func TestLogsInvalidProductId(t *testing.T) {
 	token := assistance.GetTestToken(t)
 	ctx, st := suite.New(t, configPath)
 	ctx = metadata.AppendToOutgoingContext(ctx, "authorization", token)
 
-	udpdateLog, err := st.LogsClient.GetLogs(ctx, &proto.GetLogsRequest{
-		ProductId: 1,
+	newLog, err := st.LogsClient.GetLogs(ctx, &proto.GetLogsRequest{
+		ProductId: 500,
 	})
 	require.NoError(t, err)
-	require.Equal(t, 4, len(udpdateLog.GetLogs()))
+
+	require.Equal(t, 0, len(newLog.Logs))
+}
+
+func TestGraphInvalidDate(t *testing.T) {
+	token := assistance.GetTestToken(t)
+	ctx, st := suite.New(t, configPath)
+	ctx = metadata.AppendToOutgoingContext(ctx, "authorization", token)
+
+	_, err := st.LogsClient.GetGraph(ctx, &proto.GetGraphRequest{
+		Interval: 0,
+		DateFrom: 0,
+		DateTo:   1735704000, //01.01.2025 02.00.00
+	})
+
+	require.Error(t, err, "DateFrom can't be less than minimal date of sales")
 }

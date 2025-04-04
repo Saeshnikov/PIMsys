5439e4f0d137c7f3ef3e460bd8c582914ff58831
Saeshnikov <saeshnikoff@yandex.com>
2025-04-04 21:35:06 +0300

    Add integration tests for shop

--- backend/tests/shop/shop_test.go
+++ backend/tests/shop/shop_test.go
@@ -1,6 +1,7 @@
 package tests
 
 import (
+	"context"
 	"testing"
 
 	"pim-sys/tests/assistance"
@@ -15,7 +16,7 @@ import (
 
 const configPath = "suite/config.yaml"
 
-func TestRegisterLogin_Login_HappyPath(t *testing.T) {
+func TestShop_HappyPath(t *testing.T) {
 	token := assistance.GetTestToken(t)
 	ctx, st := suite.New(t, configPath)
 	ctx = metadata.AppendToOutgoingContext(ctx, "authorization", token)
@@ -51,5 +52,72 @@ func TestRegisterLogin_Login_HappyPath(t *testing.T) {
 	require.NoError(t, err)
 
 	require.NotNil(t, respDeleteShop)
+
+}
+
+func TestShop_AlterShop_IncorrectShopId(t *testing.T) {
+	token := assistance.GetTestToken(t)
+	ctx, st := suite.New(t, configPath)
+	ctx = metadata.AppendToOutgoingContext(ctx, "authorization", token)
+
+	respAlterShop, err := st.ShopClient.AlterShop(ctx, &proto.AlterShopRequest{
+		ShopId: 99,
+		ShopInfo: &proto.ShopInfo{
+			Name:        "new-name",
+			Description: "new-description",
+			Url:         "new-url",
+		},
+	})
+	require.Nil(t, respAlterShop)
+	require.Error(t, err)
+}
+
+func TestShop_DeleteShop_IncorrectShopId(t *testing.T) {
+	token := assistance.GetTestToken(t)
+	ctx, st := suite.New(t, configPath)
+	ctx = metadata.AppendToOutgoingContext(ctx, "authorization", token)
+
+	respDeleteShop, err := st.ShopClient.DeleteShop(ctx, &proto.DeleteShopRequest{
+		ShopId: 99,
+	})
+
+	require.Nil(t, respDeleteShop)
+	require.Error(t, err)
+}
+
+func TestShop_Unauthentication(t *testing.T) {
+	ctx, st := suite.New(t, configPath)
+	ctx = context.TODO()
+
+	respNewShop, err := st.ShopClient.NewShop(ctx, &proto.NewShopRequest{
+		Name:        gofakeit.FirstName(),
+		Description: gofakeit.LastName(),
+		Url:         gofakeit.URL(),
+	})
+	require.Error(t, err)
+
+	require.Nil(t, respNewShop)
+
+	respAlterShop, err := st.ShopClient.AlterShop(ctx, &proto.AlterShopRequest{
+		ShopId: 2,
+		ShopInfo: &proto.ShopInfo{
+			Name:        "new-name",
+			Description: "new-description",
+			Url:         "new-url",
+		},
+	})
+	require.Error(t, err)
+
+	require.Nil(t, respAlterShop)
+
+	respListShops, err := st.ShopClient.ListShops(ctx, &proto.ListShopsRequest{})
+	require.Error(t, err)
+	require.Nil(t, respListShops)
+
+	respDeleteShop, err := st.ShopClient.DeleteShop(ctx, &proto.DeleteShopRequest{
+		ShopId: 2,
+	})
+	require.Error(t, err)
+	require.Nil(t, respDeleteShop)
 
 }
